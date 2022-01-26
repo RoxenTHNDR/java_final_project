229d5a72727d9e89a8a0864679f5ec6d9c535446
John Morris <roxenthndr@gmail.com>
2022-01-25 21:48:51 -0600

    Gallon to liter conversion

--- App.java
+++ App.java
@@ -1,9 +1,24 @@
+import java.text.DecimalFormat;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * App
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        Scanner kbin = new Scanner(System.in);
+        System.out.println("How many gallons?: ");
+        try{
+        double gallons = kbin.nextDouble();
+        double litpergallon = 3.78541;
+        double liters = gallons * litpergallon;
+        DecimalFormat df = new DecimalFormat("#0.00");
+        String literstring = df.format(liters);
+        System.out.println(gallons + " gallons is equivalent to " + literstring + " liters.");
+        }catch(InputMismatchException e){
+            System.out.println("Invalid Number: " + e);
+        }
+}
     }
     
-}
